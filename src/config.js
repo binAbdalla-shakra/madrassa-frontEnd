f11cd0e01342a9b1daea1266686b98f74d04cdca
Abdishakur <yareshakra6@gmail.com>
2025-07-21 15:45:23 +0300

    connect online DB

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ module.exports = {
     APP_ID: "",
   },
   api: {
-    API_URL: "http://localhost:5000/api",
-    // API_URL: "https://madrassa-backend.onrender.com/api",
+    // API_URL: "http://localhost:5000/api",
+    API_URL: "https://madrassa-backend.onrender.com/api",
 
   }
 };
